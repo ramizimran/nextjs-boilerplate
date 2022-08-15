42f7e46cb8bb64af287f00c3113183cb98dcd921
Ramiz Imran Sizar <102930858+ramizimran@users.noreply.github.com>
2022-08-16 05:27:36 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    BASE_URL: process.env.BASE_URL,
-    NEXT_API_URL: process.env.NEXT_API_URL,
-    CLOUD_URL: process.env.CLOUD_URL,
-    CLOUD_NAME: process.env.CLOUD_NAME,
-    CLOUD_PRESET: process.env.CLOUD_PRESET,
-    CLOUD_API_KEY: process.env.CLOUD_API_KEY,
-    CLOUD_API_SECRET: process.env.CLOUD_API_SECRET,
-  },
   images: {
     domains: ["res.cloudinary.com"],
   },
